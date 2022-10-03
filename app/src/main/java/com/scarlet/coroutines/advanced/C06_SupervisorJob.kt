27103ede9da000c14183add8c07f7bb5ed8433f9
Jungsun Kim <kimjs@hanyang.ac.kr>
2022-10-03 11:25:35 +0900

    AGP 7.3.0; JvmTarget=11 upgraded

--- app/src/main/java/com/scarlet/coroutines/advanced/C06_SupervisorJob.kt
+++ app/src/main/java/com/scarlet/coroutines/advanced/C06_SupervisorJob.kt
@@ -36,7 +36,7 @@ object Canceling_Parent_Cancels_All_Children {
 object Canceling_A_Child_Cancels_Only_The_Target_Child_Including_All_Its_Descendants_If_Any {
 
     @JvmStatic
-    fun main(args: Array<String>) = runBlocking{
+    fun main(args: Array<String>) = runBlocking {
 
         // What if change to Job()
         val scope = CoroutineScope(SupervisorJob())
@@ -85,5 +85,93 @@ object SupervisorJob_Child_Failure_SimpleDemo {
         joinAll(child1, child2)
 
         log("is Parent cancelled? = ${scope.coroutineContext.job.isCancelled}")
+    }
+}
+
+
+/**/
+
+object Standalone_SupervisorJob_Demo {
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking {
+
+        val supervisorJob = SupervisorJob()
+
+        val child1 = launch(supervisorJob) {
+            log("child1")
+            delay(500)
+            throw RuntimeException("Oops")
+        }.onCompletion("child1")
+
+        val child2 = launch(supervisorJob) {
+            log("child2")
+            delay(1000)
+        }.onCompletion("child2")
+
+        joinAll(child1, child2)
+        log("Done")
+    }
+}
+
+object Demo1 {
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking {
+        val scope = CoroutineScope(Job())
+
+        val job = scope.launch {
+
+            val job1 = launch {
+                log("child1")
+                println("starting Coroutine 1")
+            }.onCompletion("child1")
+
+            supervisorScope {
+                val job2 = launch {
+                    log("child2")
+                    delay(500)
+                    throw RuntimeException("oops")
+                }.onCompletion("child2")
+
+                val job3 = launch {
+                    log("child3")
+                    delay(1000)
+                    println("starting Coroutine 3")
+                }.onCompletion("child3")
+            }
+        }.onCompletion("parent")
+
+        job.join()
+        log(scope.coroutineContext.job.isCancelled)
+    }
+}
+
+// Root coroutine: exposed
+// Non-root coroutine: propagates
+object Demo2 {
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking {
+        try {
+            coroutineScope {
+//            supervisorScope {
+                val deferred = async {
+                    throw RuntimeException("Oops")
+                }.onCompletion("Child")
+
+                delay(100)
+
+                log("......... here ..........")
+
+//                try {
+//                    deferred.await()
+//                } catch (e: Exception) {
+//                    // Exception thrown in async WILL NOT be caught here
+//                    // but still will be rethrown by the coroutineScope
+//                    log("exception caught ...")
+//                }
+            }
+        } catch (ex: Exception) {
+            log("Outermost catch block")
+        }
+
     }
 }
